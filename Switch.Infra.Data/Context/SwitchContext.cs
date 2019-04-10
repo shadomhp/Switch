65237f2577c9b009124abd183821b6fcfd5421cc
Fabio Zanardo <shadom_hp@hotmail.com>
2019-04-10 00:15:04 -0300

    Organização do Infra e criação do Contexto Usuarios

---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+
+namespace Switch.Infra.Data.Context
+{
+    public class SwitchContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public SwitchContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
