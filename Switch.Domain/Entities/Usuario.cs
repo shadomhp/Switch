73bec7f21eebc5ae2850f80e4af28b73964ae2b9
Fabio Zanardo <shadom_hp@hotmail.com>
2019-04-10 00:04:03 -0300

    Criação de Pastas e Classes do Switch.Domain

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using Switch.Domain.Enums;
+using System;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public SexoEnum Sexo { get; set; }
+        public string UrlFoto { get; set; }
+        
+    }
+}
